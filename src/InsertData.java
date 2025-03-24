e7d996610cf527125df8d496ba12677dc63803ae
T Acharya <acharyatarakanta2002@gmail.com>
2025-03-24 17:51:01 +0530

    Update InsertData.java

--- src/InsertData.java
+++ src/InsertData.java
@@ -9,7 +9,7 @@ public class InsertData {
             Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/zomato_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
                 "root",
-                "Tarak@2024"
+                "XXXXXXXXX"
             );
 
             Statement statement = connection.createStatement();
